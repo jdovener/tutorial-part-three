72778d3f852ecfe41a19569a288d1955f38d494b
Jimmy <jdovener16@gmail.com>
2021-02-12 17:28:59 +0000

    changed links

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,14 +10,14 @@ export default function Layout({ children }) {
   return (
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <Link to="./" style={{ textShadow: `none`, backgroundImage: `none` }}>
           <h3 style={{ display: `inline` }}>JeemWeb</h3>
         </Link>
         <ul style={{ listStyle: `none`, float: `right` }}>
-          <ListLink to="/">Home</ListLink>
-          <ListLink to="/about/">About</ListLink>
-          <ListLink to="/contact/">Contact</ListLink>
-          <ListLink to="/project/">Project</ListLink>
+          <ListLink to="./">Home</ListLink>
+          <ListLink to="./about/">About</ListLink>
+          <ListLink to="./contact/">Contact</ListLink>
+          <ListLink to="./project/">Project</ListLink>
         </ul>
       </header>
       {children}
