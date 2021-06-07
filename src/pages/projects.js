438a68ccf47ab418f630a13cfb4b29bae9457abf
Jimmy <jdovener16@gmail.com>
2021-06-07 16:20:27 +0100

    quote app added

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,8 @@
 import React from "react"
 import Layout from "../components/layout"
 import TrackList from "../components/audio-player/track-list";
-import screenshot from "../../static/screenshot.png"
+import todo from "../../static/todo.png"
+import quote from "../../static/quote.png"
 import "../style/index.scss";
 
 const tracks=[
@@ -19,15 +20,23 @@ const tracks=[
 export default function Project() {
     return (
         <Layout>
-            <h3>1. To-dos App</h3>
+            <h3>1. Quote Generator</h3>
+            <p>An app that randomly selects from an amalgamation of my favourite quotes</p>
+            <div className="paddedDiv">
+                <a href="https://jdovener.github.io/quote-generator" target="_blank" rel="noreferrer">Click here to view live page</a>
+            </div>
+            <div className="paddedDiv">
+                <img src={quote} alt="Screenshot of Quote app"/>
+            </div>
+            <h3>2. To-dos App</h3>
             <p>An organisation app created in React</p>
             <div className="paddedDiv">
-                <a href="https://jdovener.github.io/todo" target="_blank">Click here to view live page</a>
+                <a href="https://jdovener.github.io/todo" target="_blank" rel="noreferrer">Click here to view live page</a>
             </div>
             <div className="paddedDiv">
-                <img src={screenshot} />
+                <img src={todo} alt="Screenshot of Todo app" />
             </div>
-            <h3>2. Starline</h3>
+            <h3>3. Starline</h3>
             <p>
                 A video game I am composing the music for, 
                 below are 3 of the currently available tracks.
